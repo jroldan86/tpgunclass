564bfb65c35cdab364aa33ba7aa4794a110a64e1
jroldan86 <48389716+jroldan86@users.noreply.github.com>
2019-03-27 15:12:18 -0500

    .
    .

---
+++ script.js
@@ -0,0 +1,11 @@
+// WEB API
+
+
+const item1 = document.createElement('li')
+item1.textContent = "RPA"
+
+///appenChild
+
+const lista = document.querySelector("mi lista")
+
+document.body.appendChild(item1)
